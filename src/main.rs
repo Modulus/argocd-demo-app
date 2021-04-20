4695c75b21e0737a1afbe41102caa0d5a6b65880
John Sigvald Skauge <johnsigvaldskauge@tv2.no>
2021-04-20 09:32:18 +0200

    Added failing code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+use rocket::http::Status;
+
+
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/fail1")]
+fn fail1() -> Status {
+    Status::NotAcceptable
+}
+
+#[get("/fail2")]
+fn fail2() -> Status {
+    Status::InternalServerError
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index, fail1, fail2]).launch();
 }
